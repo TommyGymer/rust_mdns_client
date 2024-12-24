4bf665c765264530c002d021dc4de739d5705664
TommyGymer <84601129+TommyGymer@users.noreply.github.com>
2024-12-24 22:55:31 +0000

    add clap cli

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,24 @@
+use clap::Parser;
 use color_eyre::Result;
 use futures_util::{pin_mut, stream::StreamExt};
 use mdns::{Record, RecordKind};
 use std::{net::IpAddr, time::Duration};
 
+/// Simple TUI for discovering mDNS capable devices
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Args {
+    /// The mDNS query, e.g., "_http._tcp.local"
+    query: String,
+}
+
 #[async_std::main]
 async fn main() -> Result<()> {
-    let stream =
-        mdns::discover::all("_formlabs_formule._tcp.local", Duration::from_secs(15))?.listen();
+    color_eyre::install()?;
+
+    let args = Args::parse();
+
+    let stream = mdns::discover::all(args.query, Duration::from_secs(15))?.listen();
     pin_mut!(stream);
 
     while let Some(Ok(response)) = stream.next().await {
