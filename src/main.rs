4387b95aad69eb36b79608a5a9a6338c560f440c
TommyGymer <84601129+TommyGymer@users.noreply.github.com>
2024-12-26 23:38:20 +0000

    add mdns thread and record mutex

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
+use async_std::task;
 use clap::Parser;
 use color_eyre::Result;
 use crossterm::event::{self, Event};
 use futures_util::{pin_mut, stream::StreamExt};
 use mdns::{discover, Record, RecordKind};
 use ratatui::{DefaultTerminal, Frame};
-use std::{net::IpAddr, time::Duration};
+use std::{
+    net::IpAddr,
+    sync::{Arc, Mutex},
+    time::Duration,
+};
 
 /// Simple TUI for discovering mDNS capable devices
 #[derive(Debug, Parser)]
@@ -22,19 +27,30 @@ async fn main() -> Result<()> {
     let args = Args::parse();
 
     let stream = discover::all(args.query, Duration::from_secs(15))?.listen();
-    pin_mut!(stream);
 
-    let result = run(terminal);
+    let records: Arc<Mutex<Vec<String>>> = Arc::new(Mutex::new(Vec::new()));
+    let records_out = records.clone();
+
+    let child = task::spawn(async move {
+        pin_mut!(stream);
+        while let Some(Ok(response)) = stream.next().await {
+            let res: Vec<(IpAddr, String)> =
+                response.records().filter_map(self::to_ip_addr).collect();
+
+            for (addr, name) in res {
+                // println!("found cast device {} at {}", name, addr);
+                records
+                    .lock()
+                    .unwrap()
+                    .push(format!("found cast device {} at {}", name, addr));
+            }
+        }
+    });
+
+    let result = run(terminal, records_out);
     ratatui::restore();
 
-    while let Some(Ok(response)) = stream.next().await {
-        let res: Vec<(IpAddr, String)> = response.records().filter_map(self::to_ip_addr).collect();
-
-        for (addr, name) in res {
-            println!("found cast device {} at {}", name, addr);
-        }
-    }
-
+    child.cancel().await;
     result
 }
 
@@ -46,7 +62,7 @@ fn to_ip_addr(record: &Record) -> Option<(IpAddr, String)> {
     }
 }
 
-fn run(mut terminal: DefaultTerminal) -> Result<()> {
+fn run(mut terminal: DefaultTerminal, records: Arc<Mutex<Vec<String>>>) -> Result<()> {
     loop {
         terminal.draw(render)?;
         if matches!(event::read()?, Event::Key(_)) {
